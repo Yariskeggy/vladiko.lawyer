502cd07bdeed8836137af042e135fe5b0edda060
Yariskeggy <razdolbaishe@gmail.com>
2024-01-21 13:30:43 +0300

    lawyer description
    work on txt cards and adaptive

--- js/lawyerCards.js
+++ js/lawyerCards.js
@@ -1,9 +1,13 @@
 const buttons = document.querySelectorAll('.lawyer__cards_col');
 const lawyerBtnFwd = document.querySelectorAll(".lawyer__Btn_Fwd");
+// const blocks = document.querySelectorAll('.lawyer__column_decsription');
+// const elems = document.querySelectorAll('.lawyer__cards_column');
 
 buttons.forEach((button) => {
     // Назначаем обработчик события "click" для каждой кнопки
     button.addEventListener('click', () => {
+
+
         // Получаем индекс кнопки
         const index = button.getAttribute('data-index');
         const indexcol = button.getAttribute('index-col');
@@ -14,13 +18,10 @@ buttons.forEach((button) => {
         const block = document.querySelector(`.lawyer__column_decsription[data-index="${index}"]`);
         console.log(block);
 
-        // Скрываем остальные блоки
-        const blocks = document.querySelectorAll('.lawyer__column_decsription');
-        const elems = document.querySelectorAll('.lawyer__cards_column');
 
         elem.style.display = 'none';
         block.style.display = 'flex';
-
+        console.log(block.style);
 
         lawyerBtnFwd.forEach((btn) => {
             btn.addEventListener('click', () => {
